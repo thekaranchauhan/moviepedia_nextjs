49f8bd1b67f3f513269355624eb4bb481dec4926
Karan Chauhan <86901403+thekaranchauhan@users.noreply.github.com>
2022-01-20 11:46:42 +0530

    Update index.tsx

--- components/organisms/DiscoverMovies/index.tsx
+++ components/organisms/DiscoverMovies/index.tsx
@@ -7,8 +7,12 @@ export default function DiscoverMovies() {
         <h2 className="fw-bold">Discover</h2>
       </div>
       <div className="discover-wrapper scroll-wrapper pb-5">
-        <DiscoverItem id={597891} />
-        <DiscoverItem id={619297} />
+        <DiscoverItem id={597895} />
+        <DiscoverItem id={619296} />
+        <DiscoverItem id={508941} />
+        <DiscoverItem id={588227} />
+        <DiscoverItem id={635532} />
+        <DiscoverItem id={525661} />
       </div>
     </div>
   );
